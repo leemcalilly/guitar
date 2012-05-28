a558eedb77dfc7ad9987ac77af05727cc7e7d6fe
Lee McAlilly <leemcalilly@gmail.com>
2012-05-28 14:23:56 -0400

    initial layout commit with favicons

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  protect_from_forgery
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Guitar::Application.routes.draw do
   # The priority is based upon order of creation:
-  # first created -> highest priority.
+  # first created -> highest priority.  
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,7 +48,7 @@ Guitar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
